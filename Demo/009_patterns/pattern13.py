c0185090019f56e9a6a8769be1ae12cd5becba2c
Mohammed Ataaur Rahaman <ataago7@gmail.com>
2020-06-11 02:06:02 +0530

    update

--- Demo/009_patterns/pattern13.py
+++ Demo/009_patterns/pattern13.py
@@ -10,3 +10,30 @@
         0101
 
 """
+
+n = int(input("Enter n: "))
+
+print("Method 1:")
+a = 1
+for i in range(1, n + 1):
+    if i % 2 == 1:
+        a = 1
+    else:
+        a = 0
+    for j in range(i):
+        print(a, end="")
+        if a == 1:
+            a = 0
+        else:
+            a = 1
+    print()
+
+
+print("\nMethod 2:")
+for i in range(1, n+1):
+    for j in range(i):
+        if (i + j) % 2 == 0:
+            print("0", end="")
+        else:
+            print("1", end="")
+    print()
